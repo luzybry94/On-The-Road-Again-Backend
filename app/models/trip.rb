02283cdaf5fd1afbee09fb81cd75d1e974450e4f
luzybry94 <luzybry94@gmail.com>
2021-05-24 16:03:38 -0700

    updates trip associations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,8 @@
 class Trip < ApplicationRecord
-    has_many_attached :images
     has_many :trips_attractions
     has_many :attractions, through: :trips_attractions
-    has_many :notes
+    has_many :trips_states
+    has_many :states, through: :trips_states
+    has_many :todos, dependent: :destroy
     belongs_to :user
 end
