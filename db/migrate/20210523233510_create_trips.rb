92f8bb1e44f5cf90b9b555e8ced1ccc74af71250
luzybry94 <luzybry94@gmail.com>
2021-05-24 16:05:23 -0700

    removes region and adds img column

--- db/migrate/20210523233510_create_trips.rb
+++ db/migrate/20210523233510_create_trips.rb
@@ -2,9 +2,9 @@ class CreateTrips < ActiveRecord::Migration[6.0]
   def change
     create_table :trips do |t|
       t.string :name
-      t.string :region
       t.date :start_date
       t.date :end_date
+      t.string :img
       t.belongs_to :user
 
       t.timestamps
