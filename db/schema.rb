5136ddfc78cf27ef9cdd90ae2149b4c0ee0b80b2
luzybry94 <luzybry94@gmail.com>
2021-05-24 16:04:46 -0700

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,65 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_013252) do
+ActiveRecord::Schema.define(version: 2021_05_24_082653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
   create_table "attractions", force: :cascade do |t|
     t.string "name"
-    t.decimal "price"
+    t.string "pricing"
     t.date "date"
-    t.bigint "location_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["location_id"], name: "index_attractions_on_location_id"
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.string "city"
-    t.string "state"
-    t.string "country"
+    t.string "img"
+    t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "notes", force: :cascade do |t|
+  create_table "states", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
     t.string "content"
-    t.bigint "user_id"
+    t.boolean "completed"
+    t.bigint "trip_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "trip_id", null: false
-    t.index ["trip_id"], name: "index_notes_on_trip_id"
-    t.index ["user_id"], name: "index_notes_on_user_id"
+    t.index ["trip_id"], name: "index_todos_on_trip_id"
   end
 
   create_table "trips", force: :cascade do |t|
     t.string "name"
-    t.string "region"
     t.date "start_date"
     t.date "end_date"
+    t.string "img"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -84,6 +60,15 @@ ActiveRecord::Schema.define(version: 2021_05_24_013252) do
     t.index ["trip_id"], name: "index_trips_attractions_on_trip_id"
   end
 
+  create_table "trips_states", force: :cascade do |t|
+    t.bigint "trip_id"
+    t.bigint "state_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["state_id"], name: "index_trips_states_on_state_id"
+    t.index ["trip_id"], name: "index_trips_states_on_trip_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -93,6 +78,4 @@ ActiveRecord::Schema.define(version: 2021_05_24_013252) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "notes", "trips"
 end
