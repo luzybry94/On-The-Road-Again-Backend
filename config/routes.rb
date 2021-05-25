68808694f126ab6f7ba56606d5fcd6c6626bb9c5
luzybry94 <luzybry94@gmail.com>
2021-05-24 17:21:07 -0700

    namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :attractions
-  resources :trips
+
+  namespace :api do
+    namespace :v1 do
+      resources :attractions
+      resources :trips
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
