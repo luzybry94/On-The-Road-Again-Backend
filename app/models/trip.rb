f925adde28a48c3903150b2404b10f4dad5c4a43
luzybry94 <luzybry94@gmail.com>
2021-05-26 02:30:44 -0700

    added date formatting method

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,4 +5,12 @@ class Trip < ApplicationRecord
     has_many :states, through: :trips_states
     has_many :todos, dependent: :destroy
     belongs_to :user
+
+    def date_start
+        self.start_date.to_formatted_s(:long) 
+    end
+
+    def date_end
+        self.end_date.to_formatted_s(:long) 
+    end
 end
