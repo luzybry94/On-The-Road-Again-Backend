3d2cd56eb4fb1400c52a34f4133367a72e669854
luzybry94 <luzybry94@gmail.com>
2021-05-26 02:32:38 -0700

    updated seed file with hosted images

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-Trip.destroy_all
-Attraction.destroy_all
-State.destroy_all
 
+Trip.destroy_all
+
+Attraction.destroy_all
+
+State.destroy_all
 
 luzy = User.create(name: 'Luzy Bryski', email: 'luzy@gmail.com', username: 'luzyb', password: 'password')
 
-west = Trip.create(name: 'West Coast Trippin', start_date: '2021-05-25', end_date: '2021-06-03', img: '/Users/luzybryski/Flatiron/code/mod5/project/On-The-Road-Again/on-the-road-again-backend/public/images/westcoast.jpg', user: luzy)
-east = Trip.create(name: 'Ny Baby!', start_date: '2021-06-15', end_date: '2021-06-25', img: '/Users/luzybryski/Flatiron/code/mod5/project/On-The-Road-Again/on-the-road-again-backend/public/images/eastcoast.jpg', user: luzy)
-coast = Trip.create(name: 'Fun In The Sun', start_date: '2021-07-05', end_date: '2021-07-20', img: '/Users/luzybryski/Flatiron/code/mod5/project/On-The-Road-Again/on-the-road-again-backend/public/images/coast.jpg', user: luzy)
+west = Trip.create(name: 'West Coast Trippin', start_date: '2021-05-25', end_date: '2021-06-03', img: 'https://res.cloudinary.com/dlhbuxc82/image/upload/v1621923590/westcoast_zqqr3b.jpg', user: luzy)
+east = Trip.create(name: 'Ny Baby!', start_date: '2021-06-15', end_date: '2021-06-25', img: 'https://res.cloudinary.com/dlhbuxc82/image/upload/v1621923589/eastcoast_mknbr9.jpg', user: luzy)
+coast = Trip.create(name: 'Fun In The Sun', start_date: '2021-07-05', end_date: '2021-07-20', img: 'https://res.cloudinary.com/dlhbuxc82/image/upload/v1621923562/coast_dwgiwr.jpg', user: luzy)
 
-west.attractions.create([{name: 'Dodger Stadium', pricing: 'Moderate', date: '2021-05-26', location: 'Los Angeles, CA', img: '/Users/luzybryski/Flatiron/code/mod5/project/On-The-Road-Again/on-the-road-again-backend/public/images/dodgerstadium.jpg'}, {name: 'Seaworld', pricing: 'Moderate', date: '2021-05-27', location: 'San Diego, CA', img: '/Users/luzybryski/Flatiron/code/mod5/project/On-The-Road-Again/on-the-road-again-backend/public/images/seaworld.jpg'}, {name: 'Golden Gate Bridge', pricing: 'Free', date: '2021-05-28', location: 'San Francisco, CA', img: '/Users/luzybryski/Flatiron/code/mod5/project/On-The-Road-Again/on-the-road-again-backend/public/images/goldengatebridge.jpg'}])
-east.attractions.create([{name: 'Statue of Liberty', pricing: 'Moderate', date: '2021-06-15', location: 'New York, NY', img: '/Users/luzybryski/Flatiron/code/mod5/project/On-The-Road-Again/on-the-road-again-backend/public/images/statueofliberty.jpg'}, {name: 'Fenway Park', pricing: 'Expensive', date: '2021-06-17', location: 'Boston, MA', img: '/Users/luzybryski/Flatiron/code/mod5/project/On-The-Road-Again/on-the-road-again-backend/public/images/fenwaypark.jpg'}])
-coast.attractions.create([{name: 'Miami Beach', pricing: 'Free', date: '2021-07-05', location: 'Miami, FL', img: '/Users/luzybryski/Flatiron/code/mod5/project/On-The-Road-Again/on-the-road-again-backend/public/images/miamibeach.jpg'}, {name: 'World of Coca-Cola', pricing: 'Moderate', date: '2021-07-07', location: 'Atlanta, GA', img: '/Users/luzybryski/Flatiron/code/mod5/project/On-The-Road-Again/on-the-road-again-backend/public/images/cocacolafactory.jpg'}])
+west.attractions.create([{name: 'Dodger Stadium', pricing: 'Moderate', date: '2021-05-26', location: 'Los Angeles, CA', img: 'https://res.cloudinary.com/dlhbuxc82/image/upload/v1621923579/dodgerstadium_d7wwxl.jpg'}, {name: 'Seaworld', pricing: 'Moderate', date: '2021-05-27', location: 'San Diego, CA', img: 'https://res.cloudinary.com/dlhbuxc82/image/upload/v1621923590/seaworld_h0hnud.jpg'}, {name: 'Golden Gate Bridge', pricing: 'Free', date: '2021-05-28', location: 'San Francisco, CA', img: 'https://res.cloudinary.com/dlhbuxc82/image/upload/v1621923603/goldengatebridge_rqekok.jpg'}])
+east.attractions.create([{name: 'Statue of Liberty', pricing: 'Moderate', date: '2021-06-15', location: 'New York, NY', img: 'https://res.cloudinary.com/dlhbuxc82/image/upload/v1621923564/statueofliberty_su7wph.jpg'}, {name: 'Fenway Park', pricing: 'Expensive', date: '2021-06-17', location: 'Boston, MA', img: 'https://res.cloudinary.com/dlhbuxc82/image/upload/v1621923614/fenwaypark_wfbsqp.jpg'}])
+coast.attractions.create([{name: 'Miami Beach', pricing: 'Free', date: '2021-07-05', location: 'Miami, FL', img: 'https://res.cloudinary.com/dlhbuxc82/image/upload/v1621923562/miamibeach_tsdvtd.jpg'}, {name: 'World of Coca-Cola', pricing: 'Moderate', date: '2021-07-07', location: 'Atlanta, GA', img: 'https://res.cloudinary.com/dlhbuxc82/image/upload/v1621923565/cocacolafactory_tkeard.jpg'}])
 
 west.todos.create([{content: 'Food for the way', completed: false}, {content: 'Book hotel in SD', completed: false}])
 east.todos.create(content: 'Find kosher restaurant in Manhattan', completed: false)
