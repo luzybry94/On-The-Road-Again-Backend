e94f99d572b2e5177a58c2210a1aa2abc74eb445
luzybry94 <luzybry94@gmail.com>
2021-05-26 02:30:32 -0700

    added date formatting method

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,4 +1,8 @@
 class Attraction < ApplicationRecord
     has_many :trips_attractions
     has_many :trips, through: :trips_attractions
+
+    def date_lg
+        self.date.to_formatted_s(:long) 
+    end
 end
