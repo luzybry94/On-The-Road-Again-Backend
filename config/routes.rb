be027dd3c4780ca3ac9e5bbc69f4672d49360692
luzybry94 <luzybry94@gmail.com>
2021-05-28 13:02:08 -0700

    updates to nested routes and adds login routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :attractions
-      resources :trips
+      resources :users, only: [:create] do
+        resources :trips, only: [:index, :show, :create] do
+          resources :attractions, only: [:index, :create]
+        end
+      end
+      get '/states', to: 'states#index'
+      post '/login', to: 'auth#create'
+      get '/autologin', to: 'auth#autologin'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
