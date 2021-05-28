0d63e5cd9292dd800307b383c38c06b8780dfa61
luzybry94 <luzybry94@gmail.com>
2021-05-28 13:01:21 -0700

    removes unnnecessary attributes

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,7 +1,4 @@
 class TripSerializer < ActiveModel::Serializer
   attributes :id, :name, :date_start, :date_end, :img
-  has_many :attractions
   has_many :states
-  has_many :todos
-  belongs_to :user
 end
