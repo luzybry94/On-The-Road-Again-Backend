2b183d005e940b7c72e05d9bd83b5230a351fdf0
luzybry94 <luzybry94@gmail.com>
2021-05-28 19:10:17 -0700

    adds logged_in to json response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
       @user = User.create(user_params)
       if @user.valid?
         @token = encode_token(user_id: @user.id)
-        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        render json: {status: 201, user: UserSerializer.new(@user), jwt: token, logged_in: true}
       else
         render json: { error: 'Account with that email already exists' }, status: :not_acceptable
       end
