3811cb63c3e547a936142dab3c354b1ca810aa32
luzybry94 <luzybry94@gmail.com>
2021-06-01 03:27:06 -0700

    adds signup route and nests attractions/todos under trips

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create] do
-        resources :trips, only: [:index, :show, :create] do
-          resources :attractions, only: [:index, :create]
-        end
+      resources :trips, only: [:index, :show, :create] do
+        resources :attractions, only: [:index, :create]
+        resources :todos, only: [:create, :update]
       end
+      
+      post '/signup', to: 'users#create'
       get '/states', to: 'states#index'
       post '/login', to: 'auth#create'
       get '/autologin', to: 'auth#autologin'
