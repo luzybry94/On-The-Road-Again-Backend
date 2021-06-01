ade28a6a672b72107aa82bc6ef885b6e76b3ae11
luzybry94 <luzybry94@gmail.com>
2021-06-01 03:23:53 -0700

    updates trips association to attractions

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,5 @@
 class Trip < ApplicationRecord
-    has_many :trips_attractions
-    has_many :attractions, through: :trips_attractions
+    has_many :attractions
     has_many :trips_states
     has_many :states, through: :trips_states
     has_many :todos, dependent: :destroy
