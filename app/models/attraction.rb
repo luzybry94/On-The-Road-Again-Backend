3c3ad285a52037586ed50189bcf7d11195d39323
luzybry94 <luzybry94@gmail.com>
2021-06-01 03:23:30 -0700

    updates attraction association

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,6 +1,7 @@
 class Attraction < ApplicationRecord
-    has_many :trips_attractions
-    has_many :trips, through: :trips_attractions
+   belongs_to :trip
+    
+    
 
     def date_lg
         self.date.to_formatted_s(:long) 
