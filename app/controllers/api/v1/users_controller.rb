c7b4bae6d982e11fefc0d40172ea6e32c84c3b4c
luzybry94 <luzybry94@gmail.com>
2021-06-01 03:23:13 -0700

    removes @ from token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < ApplicationController
     def create
       @user = User.create(user_params)
       if @user.valid?
-        @token = encode_token(user_id: @user.id)
+        token = encode_token(user_id: @user.id)
         render json: {status: 201, user: UserSerializer.new(@user), jwt: token, logged_in: true}
       else
         render json: { error: 'Account with that email already exists' }, status: :not_acceptable
