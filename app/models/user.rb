204830f6d67ae1d26c8cce3a51631761cf08786f
luzybry94 <luzybry94@gmail.com>
2021-06-04 19:29:10 -0700

    replaces email validation with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :trips
-    validates :email, uniqueness: { case_sensitive: false }
+    validates :username, uniqueness: true
     
     has_secure_password
 end
