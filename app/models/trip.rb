982ab01d340f442d2aa3e29c22f3441427d562cb
luzybry94 <luzybry94@gmail.com>
2021-06-04 19:28:39 -0700

    removes date formatting method and adds destroy dependency on attractions

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,15 +1,7 @@
 class Trip < ApplicationRecord
-    has_many :attractions
+    has_many :attractions, dependent: :destroy
     has_many :trips_states
     has_many :states, through: :trips_states
     has_many :todos, dependent: :destroy
     belongs_to :user
-
-    def date_start
-        self.start_date.to_formatted_s(:long) 
-    end
-
-    def date_end
-        self.end_date.to_formatted_s(:long) 
-    end
 end
