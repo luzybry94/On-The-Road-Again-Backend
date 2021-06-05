a52ed0db544e2c7e12158dc4c2baa2da94566b83
luzybry94 <luzybry94@gmail.com>
2021-06-04 19:27:08 -0700

    adds setTodo before action

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,23 +1,42 @@
 class Api::V1::TodosController < ApplicationController
-    before_action :set_trip, only: [:update, :destroy]
+    before_action :set_trip, only: [:update, :destroy, :create]
+    before_action :set_todo, only: [:update, :destroy]
+
+    def create
+      @todo = Todo.new(todo_params)
+      if @todo.save
+        render json: @trip
+      else
+        render json: @todo.errors, status: :unprocessable_entity
+      end
+    end
 
     def update
-        @todo = Todo.find(params[:id])
         if @todo.update(todo_params)
           render json: @trip
         else
           render json: @todo.errors, status: :unprocessable_entity
         end
+    end
+
+    def destroy
+      if @todo.destroy
+        render json: @trip
+      else
+        render json: {error: "Todo not found"}
       end
+    end
   
     private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_trip
       @trip = Trip.find(params[:trip_id])
     end
 
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
 
-    # Only allow a trusted parameter "white list" through.
     def todo_params
       params.permit(:content, :completed, :trip_id)
     end
